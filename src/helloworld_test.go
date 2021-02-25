73c7861564aae49b7d100e132722de80d682521c
Anthony Do <anthonydo@gmail.com>
2021-02-25 17:17:31 -0500

    Update helloworld_test.go

--- src/helloworld_test.go
+++ src/helloworld_test.go
@@ -1,11 +1,5 @@
 package main
 
-import "testing"
-
-func TestCalculatePi(t *testing.T) {
-    var pi float64
-    pi = calculatePi(20000000)
-    if pi < 3.1415926 || pi >= 3.1415927 {
-       t.Errorf("Value %.10f is incorrect", pi)
-    }
+func main() {
+	print("hello, world\n")
 }
